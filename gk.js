0545cfd7ef8eb89564ddbc179743151ef4a18471
blogth3pr0 <65220230+blogth3pr0@users.noreply.github.com>
2020-06-25 10:00:41 -0700

    Update gk.js

--- gk.js
+++ gk.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
       {
         onClickLink: 'http://web.archive.org/web/20130810071810im_/http://sea.sy/uploaded_files/articles/1964.jpg',
         imgageSource: 'http://web.archive.org/web/20130810071810im_/http://sea.sy/uploaded_files/articles/1964.jpg',
-        title: 'Tài khoản Twitter Financial Times bị hack bởi th3pr0 (The Pro) là thành viên của Syrian Electronic Army (SEA) ( 17/05/2013) '
+        title: 'Tài khoản Twitter ITV NEWS London bị hack bởi th3pr0 (The Pro) là thành viên của Syrian Electronic Army (SEA) ( 24/05/2013) '
       },
       {
         onClickLink: 'http://web.archive.org/web/20130810035900im_/http://sea.sy/uploaded_files/articles/1968.jpg',
         imgageSource: 'http://web.archive.org/web/20130810035900im_/http://sea.sy/uploaded_files/articles/1968.jpg',
-        title: 'Ứng dụng  Sky News bị hack bởi th3pr0 (The Pro)  Syrian Electronic Army (SEA) (19/05/2013)'
+        title: 'Ứng dụng  Sky News bị hack bởi th3pr0 (The Pro)  Syrian Electronic Army (SEA) (24/05/2013)'
       },
       {
         onClickLink: 'http://web.archive.org/web/20141128215319im_/http://sea.sy/uploaded_files/articles/1951.jpg',
@@ -19,17 +19,17 @@ document.addEventListener("DOMContentLoaded", function() {
       {
         onClickLink: 'http://web.archive.org/web/20130810052628im_/http://sea.sy/uploaded_files/articles/1957.jpg',
         imgageSource: 'http://web.archive.org/web/20130810052628im_/http://sea.sy/uploaded_files/articles/1957.jpg',
-        title: ' Tài khoản Twitter The Onion bị hack bởi th3pr0 (The Pro) Syrian Electronic Army (SEA) (05/6/2013)'
+        title: ' Tài khoản Twitter The Onion bị hack bởi th3pr0 (The Pro) Syrian Electronic Army (SEA) (06/5/2013)'
       },
       {
         onClickLink: 'https://web.archive.org/web/20130810043247im_/http://sea.sy/uploaded_files/articles/1979.jpg',
         imgageSource: 'https://web.archive.org/web/20130810043247im_/http://sea.sy/uploaded_files/articles/1979.jpg',
-        title: 'Cơ sở dữ liệu True Caller bị hack, Trang quản trị Wordpress True Caller đã bị chiếm quyền điều khiển bởi Th3Pr0 (SEA)  (17/07/2013)'
+        title: 'Cơ sở dữ liệu True Caller bị hack, Trang quản trị Wordpress True Caller đã bị chiếm quyền điều khiển bởi Th3Pr0 (SEA)  (16/07/2013)'
       },
       {
         onClickLink: 'http://web.archive.org/web/20130809222604im_/http://sea.sy/uploaded_files/articles/1967.jpg',
         imgageSource: 'http://web.archive.org/web/20130809222604im_/http://sea.sy/uploaded_files/articles/1967.jpg',
-        title: 'Tài khoản Twitter  Financial Times bị hack bởi Th3Pr0  (SEA) (17/05/2013)'
+        title: 'Tài khoản Twitter  Financial Times bị hack bởi Th3Pr0  (SEA) (16/05/2013)'
       }
       
       
